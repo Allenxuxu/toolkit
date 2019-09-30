593ef657bbee14a8271c011f645052a330960240
Allenxuxu <120582243@qq.com>
2019-09-30 11:03:15 +0800

    add byte string conver

---
+++ byte/benchmark_byte_test.go
@@ -0,0 +1,34 @@
+package byte
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+var str = strings.Repeat("test", 1024)
+var bs = bytes.Repeat([]byte("test"), 1024)
+
+func BenchmarkStringToBytes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = StringToBytes(str)
+	}
+}
+
+func BenchmarkSTDStringToBytes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = []byte(str)
+	}
+}
+
+func BenchmarkBytesToString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = BytesToString(bs)
+	}
+}
+
+func BenchmarkSTDBytesToString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = string(bs)
+	}
+}
